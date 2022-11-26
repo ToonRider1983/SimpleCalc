f461250bfc76f3b7515556ca4cdb750331dc4320
Jiradej Kerdsiri <106429190+ToonRider1983@users.noreply.github.com>
2022-11-26 21:19:23 +0700

    444

--- js/main.js
+++ js/main.js
@@ -1,24 +1,42 @@
+// Simple Calculator 
+let Number1 = 0, Number2 = 0;
+let charOperator = "";
 
-let Number1, Number2;
-let Operator;
-
-// This is 'Original Function' in JS.
-function setOperator(objValue, _Operator) {
-  Number1 = objValue;
-  Operator = _Operator;
-  document.getElementById('numInput').value = "";
-  document.getElementById('showAnswer').innerHTML += Number1 + " " + Operator;
+// This is the original function pattern in JS.
+function setOperator(Operator) {
+  charOperator = Operator;
+  document.getElementById('txtOperator').value = Operator;
 }
 
-Sum=() => {
-    switch(Operator) {
-        case '+' : return Add(Number1, Number2); break;
-        
-    }
+// This is the arrow function pattern in JS.
+Add=()=> { return Number1 + Number2; }
+Minus=()=> { return Number1 - Number2; }
+Multiple=()=> { return Number1 * Number2; }
+Divide=()=> { return Number1 / Number2; }
+
+// This is the arrow function pattern in JS.
+ResetAll=()=> { 
+  Number1 = Number2 = 0;
+  charOperator = "";
+  document.getElementById('numInput1').value
+    = document.getElementById('numInput2').value
+    = document.getElementById('numAnswer').value = 0;
+  
+  document.getElementById('txtOperator').value = "";
 }
 
-Add=(Num1, Num2) => { return Number1 + Number2; }
+// This is the arrow function pattern in JS.
+Calculation=(NumFirst, NumSecond)=> {
+  Number1 = NumFirst;
+  Number2 = NumSecond;
 
+  document.getElementById('numAnswer').value = "0";
 
+  switch(charOperator) {
+    case '+' :  document.getElementById('numAnswer').value = Add(); break;
+    case '-' :  document.getElementById('numAnswer').value = Minus(); break;
+    case '*' :  document.getElementById('numAnswer').value = Multiple(); break;
+    case '/' :  document.getElementById('numAnswer').value = Divide(); break;
+  }
 
-console.log(Sum());
+}
