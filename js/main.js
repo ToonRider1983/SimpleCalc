305a8ccdc20c1268c4e5265a551b5f3dc9e3023f
toonrider2526@gmail.com <toonrider2526@gmail.com>
2022-11-27 07:20:41 +0700

    rithfjd

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
 // Simple Calculator 
-var Number1 = 0.0, Number2 = 0.0;
-let charOperator = "";
+let Number1 = 0.0, Number2 = 0.0
+let charOperator
 
 // This is the original function pattern in JS.
 function setOperator(Operator) {
-  charOperator = Operator;
-  document.getElementById('txtOperator').value = Operator;
+  charOperator = Operator
+  document.getElementById('txtOperator').value = Operator
 }
 
 // This is the arrow function pattern in JS.
-Add=()=> { return Number(Number1 + Number2); }
-Minus=()=> { return Number1 - Number2; }
-Multiple=()=> { return Number1 * Number2; }
-Divide=()=> { return Number1 / Number2; }
+Add=()=> { return Number1 + Number2 }
+Minus=()=> { return Number1 - Number2 }
+Multiple=()=> { return Number1 * Number2 }
+Divide=()=> { return Number1 / Number2 }
 
 // This is the arrow function pattern in JS.
 ResetAll=()=> { 
-  Number1 = Number2 = 0;
-  charOperator = "";
+  Number1 = Number2 = 0
+  charOperator = ""
 
   document.getElementById('numInput1').value
     = document.getElementById('numInput2').value
-    = document.getElementById('numAnswer').value = 0;
+    = document.getElementById('numAnswer').value = NaN
   
-  document.getElementById('txtOperator').value = "";
+  document.getElementById('txtOperator').value = ""
 }
 
 // This is the arrow function pattern in JS.
 Calculation=(NumFirst, NumSecond)=> {
-  Number1 = NumFirst;
-  Number2 = NumSecond;
+  Number1 = NumFirst
+  Number2 = NumSecond
 
-  document.getElementById('numAnswer').value = "0";
+  document.getElementById('numAnswer').value = 0
 
   switch(charOperator) {
-    case '+' :  document.getElementById('numAnswer').value = Add(); break;
-    case '-' :  document.getElementById('numAnswer').value = Minus(); break;
-    case '*' :  document.getElementById('numAnswer').value = Multiple(); break;
-    case '/' :  document.getElementById('numAnswer').value = Divide(); break;
+    case '+' : document.getElementById('numAnswer').value = Add(); break
+    case '-' : document.getElementById('numAnswer').value = Minus().toFixed(2); break
+    case '*' : document.getElementById('numAnswer').value = Multiple().toFixed(2); break
+    case '/' : document.getElementById('numAnswer').value = Divide().toFixed(2); break
   }
 }
